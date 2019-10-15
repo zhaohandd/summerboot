4d0a43494d15a08efddf13b7d7ec38c1e2d5a546
xhzhao <xhzhao@zju.edu.cn>
2019-10-15 20:24:17 +0800

    初始化框架

---
+++ src/main/java/com/xhzhao/summerboot/HelperLoader.java
@@ -0,0 +1,26 @@
+package com.xhzhao.summerboot;
+
+import com.xhzhao.summerboot.helper.BeanHelper;
+import com.xhzhao.summerboot.helper.ClassHelper;
+import com.xhzhao.summerboot.helper.ControllerHelper;
+import com.xhzhao.summerboot.helper.IocHelper;
+import com.xhzhao.summerboot.util.ClassUtil;
+
+/**
+ * @author xhzhao
+ */
+public final class HelperLoader {
+
+    public static void init() {
+        Class<?>[] classList = {
+                BeanHelper.class,
+                ClassHelper.class,
+                ControllerHelper.class,
+                IocHelper.class
+        };
+
+        for (Class<?> cls : classList) {
+            ClassUtil.loadClass(cls.getName(), false);
+        }
+    }
+}
