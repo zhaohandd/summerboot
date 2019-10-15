a7ac41dab258fc58a979a9427b16eec6c4884d81
xhzhao <xhzhao@zju.edu.cn>
2019-10-15 19:37:11 +0800

    加载Controller

---
+++ src/main/java/com/xhzhao/summerboot/bean/Handler.java
@@ -0,0 +1,28 @@
+package com.xhzhao.summerboot.bean;
+
+import java.lang.reflect.Method;
+
+/**
+ * @author xhzhao
+ */
+public class Handler {
+
+    private Class<?> controllerClass;
+
+    private Method actionMethod;
+
+    public Handler(Class<?> controllerClass, Method actionMethod) {
+        this.controllerClass = controllerClass;
+        this.actionMethod = actionMethod;
+    }
+
+    public Class<?> getControllerClass() {
+        return controllerClass;
+    }
+
+    public Method getActionMethod() {
+        return actionMethod;
+    }
+
+
+}
---
+++ src/main/java/com/xhzhao/summerboot/helper/ControllerHelper.java
@@ -0,0 +1,54 @@
+package com.xhzhao.summerboot.helper;
+
+import com.xhzhao.summerboot.annotation.Action;
+import com.xhzhao.summerboot.bean.Handler;
+import com.xhzhao.summerboot.bean.Request;
+import com.xhzhao.summerboot.util.ArrayUtil;
+import com.xhzhao.summerboot.util.CollectionUtil;
+
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author xhzhao
+ */
+public final class ControllerHelper {
+
+    private static final Map<Request, Handler> ACTION_MAP = new HashMap<>();
+
+    static {
+        Set<Class<?>> controllerClassSet = ClassHelper.getControllerClassSet();
+        if (CollectionUtil.isNotEmpty(controllerClassSet)) {
+            for (Class<?> controllerClass : controllerClassSet) {
+                Method[] methods = controllerClass.getDeclaredMethods();
+                if (ArrayUtil.isNotEmpty(methods)) {
+                    for (Method method : methods) {
+                        if (method.isAnnotationPresent(Action.class)) {
+                            Action action = method.getAnnotation(Action.class);
+                            String mapping = action.value();
+                            if (mapping.matches("\\w+:/\\w*")) {
+                                String[] array = mapping.split(":");
+                                if (ArrayUtil.isNotEmpty(array) && array.length==2) {
+                                    String requestMethod = array[0];
+                                    String requestPath = array[1];
+                                    Request request = new Request(requestMethod, requestPath);
+                                    Handler handler = new Handler(controllerClass, method);
+                                    ACTION_MAP.put(request, handler);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public static Handler getHandler(String requestMethod, String requestPath) {
+        Request request = new Request(requestMethod, requestPath);
+        return ACTION_MAP.get(request);
+    }
+}
+
+
